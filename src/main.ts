55576eadede17de548923bc84da9061b8f7e89ac
EkaanshArora <ekaansh68@gmail.com>
2023-09-13 15:24:47 +0530

    chore: cleanup and beta update

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,68 @@
 import "./style.css";
-import AgoraRTM, { type RTMClient } from "agora-rtm";
+import AgoraRTM, { type RTMClient } from "agora-rtm-sdk";
 
 let signalingEngine: RTMClient;
 
 const joinButton = document.getElementById("join") as HTMLButtonElement;
 const sendButton = document.getElementById("send") as HTMLButtonElement;
-const messageContainer = document.getElementById(
-	"messageContainer"
-) as HTMLDivElement;
 
 joinButton.onclick = async () => {
-	const { appId, channelName, token, uid } = getConfig();
-	signalingEngine = new AgoraRTM.RTM(appId, String(uid), { token: token });
-	signalingEngine.addEventListener({
-		message: (eventArgs) => {
-			addMessageToDom(`${eventArgs.publisher}: ${eventArgs.message}`);
-		},
-		status: (e) => {
-			document.getElementById("status")!.innerText = e.state;
-		},
-	});
-	await signalingEngine.login();
-	signalingEngine.subscribe(channelName);
+  const { appId, channelName, token, uid } = getConfig();
+  signalingEngine = new AgoraRTM.RTM(appId, String(uid), { token: token });
+  signalingEngine.on("message", (eventArgs) => {
+    addMessageToDom(`${eventArgs.publisher}: ${eventArgs.message}`);
+  });
+  signalingEngine.on("status", (e) => {
+    document.getElementById("status")!.innerText = e.state;
+  });
+  await signalingEngine.login();
+  signalingEngine.subscribe(channelName);
 };
 
 sendButton.onclick = async () => {
-	const { channelName, message } = getChannelAndMessage();
-	await signalingEngine.publish(channelName, message);
-	addMessageToDom("Me: " + message);
+  const { channelName, message } = getChannelAndMessage();
+  await signalingEngine.publish(channelName, message);
+  addMessageToDom("Me: " + message);
 };
 
 /* utils */
 const getConfig = () => {
-	const AppID = (document.getElementById("AppID") as HTMLInputElement).value;
-	const channelName = (document.getElementById("channel") as HTMLInputElement)
-		.value;
-	const uid = (document.getElementById("uid") as HTMLInputElement).value;
-	const token = (document.getElementById("token") as HTMLInputElement).value;
-	const config = {
-		appId: AppID,
-		channelName: channelName,
-		uid: parseInt(uid),
-		token: token,
-	};
-	if (!config.appId || !config.channelName) {
-		alert("Please input all the fields");
-		throw new Error("Please input all the fields");
-	}
-	return config;
+  const AppID = document.getElementById("AppID") as HTMLInputElement;
+  const channelName = document.getElementById("channel") as HTMLInputElement;
+  const uid = document.getElementById("uid") as HTMLInputElement;
+  const token = document.getElementById("token") as HTMLInputElement;
+  const config = {
+    appId: AppID.value,
+    channelName: channelName.value,
+    uid: parseInt(uid.value),
+    token: token.value,
+  };
+  if (!config.appId || !config.channelName) {
+    alert("Please input all the fields");
+    throw new Error("Please input all the fields");
+  }
+  AppID.disabled = true;
+  channelName.disabled = true;
+  uid.disabled = true;
+  token.disabled = true;
+  return config;
 };
 
 const getChannelAndMessage = () => {
-	const message = (
-		document.getElementById("message") as HTMLInputElement
-	).value.trim();
-	const channelName = (document.getElementById("channel") as HTMLInputElement)
-		.value;
-	return {
-		message: message,
-		channelName: channelName,
-	};
+  const message = (
+    document.getElementById("message") as HTMLInputElement
+  ).value.trim();
+  const channelName = (document.getElementById("channel") as HTMLInputElement)
+    .value;
+  return {
+    message: message,
+    channelName: channelName,
+  };
 };
 
 const addMessageToDom = (message: string) => {
-	const messageRes = document.createElement("div");
-	messageRes.innerText = message as string;
-	messageContainer.appendChild(messageRes);
+  const messageRes = document.createElement("div");
+  messageRes.innerText = message as string;
+  const container = document.getElementById("messages") as HTMLDivElement;
+  container.appendChild(messageRes);
 };
